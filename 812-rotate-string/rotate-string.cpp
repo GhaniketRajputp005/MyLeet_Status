764c65f09d6172d1f9428c83e1d99b0ca05b99ce
Ghaniket Rajput codes <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-02-24 12:45:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (24.34%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,40 +1,55 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) return 0;
+
        
-     int size = s.size();
-     vector<int>v;
-     for(int i =0; i< size;i++){
-         if(s[0] ==goal[i]){
-             v.push_back(i);
-         }
-     }
 
-    if(v.empty()){
-        return 0;
-    }
-   
-    int i = 0,idx = 0, j = v[idx];
-     while(i!=size){
-         if(s[i] == goal[j]){
-             i++;
-             j++;
-             if(j == size){
-                 j = 0;
-             }
-         }
-         else{
-             if(v.size()!=1 && idx != v.size()-1){
-                 j = v[++idx];
-                 i = 0;
-             }
-             else{
-                 return 0;
-             }
+   s+=s;
+   int i = 0,j =0,temp  = 0;;
+   while(i < s.size()  && j < goal.size()){
+       if(s[i] == goal[j]){
+           i++;
+           j++;
+       }
+       else{
+           i = temp++;
+           j = 0;
+       }
 
-         }
-     }
-     return 1;
-   
+   }
+   if(j == goal.size()) return 1;
+   return 0;
+
+        // int size = s.size();
+        // vector<int> v;
+        // for (int i = 0; i < size; i++) {
+        //     if (s[0] == goal[i]) {
+        //         v.push_back(i);
+        //     }
+        // }
+
+        // if (v.empty()) {
+        //     return 0;
+        // }
+
+        // int i = 0, idx = 0, j = v[idx];
+        // while (i != size) {
+        //     if (s[i] == goal[j]) {
+        //         i++;
+        //         j++;
+        //         if (j == size) {
+        //             j = 0;
+        //         }
+        //     } else {
+        //         if (v.size() != 1 && idx != v.size() - 1) {
+        //             j = v[++idx];
+        //             i = 0;
+        //         } else {
+        //             return 0;
+        //         }
+        //     }
+        // }
+        // return 1;
     }
 };
