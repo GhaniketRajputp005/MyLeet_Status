e2180a57311e2538b0e6f6d78fdbf7166f32efb4
Ghaniket Rajput codes <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-02-26 09:45:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (25.40%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -14,16 +14,64 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
 
-        if (p == NULL && q == NULL) {
+        ////////////    BFS     //////////////
+        if(p == NULL && q == NULL){
             return true;
         }
-
-        else if (p == NULL || q == NULL) {
+        else if(p == NULL || q == NULL){
             return false;
         }
 
-        if (p->val != q->val)
-            return false;
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+
+        q1.push(p);
+        q2.push(q);
+
+        while (!q1.empty() && !q2.empty()) {
+
+            TreeNode* node1 = q1.front();
+            q1.pop();
+            TreeNode* node2 = q2.front();
+            q2.pop();
+
+            if (node1->val != node2->val)
+                return false;
+           
+
+           if(node1->left != NULL && node2->left != NULL){
+               q1.push(node1->left);
+               q2.push(node2->left);
+           }
+           else if(node1->left != NULL || node2->left != NULL){
+               return false;
+           }
+
+           if(node1->right != NULL && node2->right != NULL){
+               q1.push(node1->right);
+               q2.push(node2->right);
+           }
+           else if(node1->right != NULL || node2->right != NULL){
+               return false;
+           }
+
+
+        }
+
+ return true;
+        ///////////   DFS(preorder traversal)    //////////////
+
+        // if (p == NULL && q == NULL) {
+        //     return true;
+        // }
+
+        // else if (p == NULL || q == NULL) {
+        //     return false;
+        // }
+
+        // if (p->val != q->val)
+        //     return false;
+        // return isSameTree(p->left, q->left) && isSameTree(p->right,
+        // q->right);
     }
 };
