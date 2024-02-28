ad3cb8ce27f036d54f6b880b54073c2c7c19d0d6
Ghaniket Rajput codes <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-02-28 22:56:47 +0530

    Time: 15 ms (15.35%) | Memory: 20.3 MB (63.53%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -12,27 +12,52 @@
  */
 class Solution {
 public:
-    void dfs (TreeNode* root, int* value, int depth, int* maxdepth) {
+    ///////////       DFS ///////////////////////
 
-        if (root == NULL)
-             { return; }
+    // void dfs (TreeNode* root, int* value, int depth, int* maxdepth) {
+
+    //     if (root == NULL)
+    //          { return; }
 
       
-        if (depth >  * maxdepth)
-             {
-                *maxdepth = depth;
-                  * value = root->val;
+    //     if (depth >  * maxdepth)
+    //          {
+    //             *maxdepth = depth;
+    //               * value = root->val;
+    //         }
+
+    //     dfs(root->left, value, depth+1, maxdepth);
+
+    //     dfs(root->right, value, depth+1, maxdepth);
+
+    // }
+
+
+    int bfs(TreeNode* root){
+        queue<TreeNode*>q;
+        q.push(root);
+        int result;
+         TreeNode* n;
+        while(!q.empty()){
+           n = q.front();
+            q.pop();
+            result = n->val;
+            if(n->right ){
+                q.push(n->right);
             }
-
-        dfs(root->left, value, depth+1, maxdepth);
-
-        dfs(root->right, value, depth+1, maxdepth);
-
+            if(n->left){
+                q.push(n->left);
+            }
+            
+        }
+        return result;
     }
 
     int findBottomLeftValue(TreeNode* root) {
-        int value = 0, depth = 0, maxdepth = -1;
-        dfs (root, &value, 0, &maxdepth);
+        // int value = 0, depth = 0, maxdepth = -1;
+        // dfs (root, &value, 0, &maxdepth);
+
+        int value = bfs(root);
         return value;
     }
 };
