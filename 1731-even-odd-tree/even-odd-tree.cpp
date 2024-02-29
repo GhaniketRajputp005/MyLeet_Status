f24c6228d90c9ec9376518e2d93fa0b1674b5234
Ghaniket Rajput codes <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-01 04:02:32 +0530

    Time: 189 ms (86.52%) | Memory: 149.1 MB (97.19%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.cpp
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -1,15 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
     bool isEvenOddTree(TreeNode* root) {
@@ -20,12 +8,7 @@ public:
             int n = q.size(), max = INT_MIN, min = INT_MAX;
             ctr++;
             while (n--) {
-                if (q.front()->left) {
-                    q.push(q.front()->left);
-                }
-                if (q.front()->right) {
-                    q.push(q.front()->right);
-                }
+                
                 if (ctr % 2 == 0) {
                     if (q.front()->val % 2 != 0 && q.front()->val > max) {
                         max = q.front()->val;
@@ -38,6 +21,12 @@ public:
                     } else {
                         return false;
                     }
+                }
+                if (q.front()->left) {
+                    q.push(q.front()->left);
+                }
+                if (q.front()->right) {
+                    q.push(q.front()->right);
                 }
 
                 q.pop();
