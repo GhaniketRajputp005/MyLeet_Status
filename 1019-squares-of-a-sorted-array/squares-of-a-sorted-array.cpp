4c247394c911450c566f498fa93f396743e6a8cb
Ghaniket Rajput codes <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-02 09:16:33 +0530

    Time: 20 ms (78.12%) | Memory: 28.2 MB (67.40%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -2,19 +2,34 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         
+        int n = nums.size();
+        int left = 0, right = n - 1;
+        vector<int>v(n);
+        for(int i = n -1; i >= 0; i--){
+            if(abs(nums[left]) > abs(nums[right])){
+                v[i] = abs(nums[left])*abs(nums[left]);
+                left++;
+     
+            }
+            else{
+                 v[i] = abs(nums[right])*abs(nums[right]);
+                right--;
+            }
+        }
+        return v;
         
-        int max;
-        abs(nums[0]) > abs(nums[nums.size()-1]) ? max = abs(nums[0])+1 : max = abs(nums[nums.size()-1])+1;
+    //     int max;
+    //     abs(nums[0]) > abs(nums[nums.size()-1]) ? max = abs(nums[0])+1 : max = abs(nums[nums.size()-1])+1;
 
-        vector<int>v(max,0),ans;
+    //     vector<int>v(max,0),ans;
 
-        for(int i = 0; i< nums.size(); i++){
-            v[abs(nums[i])]++;
+    //     for(int i = 0; i< nums.size(); i++){
+    //         v[abs(nums[i])]++;
           
-        }
-        for(int i = 0; i<max; i++){
-            if(v[i] >= 1) while(v[i]--)  ans.push_back(i*i);
-        }
-       return ans;
+    //     }
+    //     for(int i = 0; i<max; i++){
+    //         if(v[i] >= 1) while(v[i]--)  ans.push_back(i*i);
+    //     }
+    //    return ans;
     }
 };
