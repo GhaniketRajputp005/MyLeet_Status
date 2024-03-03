f7cb4c98f303913bb2805a84f700377f1e8a4ea6
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-03 22:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (34.69%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -10,15 +10,32 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
 
-        unordered_map<ListNode*, int >umap;
+        ListNode *fp = head, *sp = head, *check = head;
 
-        while(head!=NULL){
+        while(fp != NULL && fp->next != NULL){
 
-            umap[head]++;
-            if(umap[head]==2) return head;
-            head = head->next;
+
+            fp = fp->next->next;
+            sp = sp->next;
+            if(fp == sp ){
+                while(sp != check){
+                    sp= sp->next;
+                    check = check->next;
+                }
+                return sp;
+            }
         }
         return NULL;
+
+        // unordered_map<ListNode*, int >umap;
+
+        // while(head!=NULL){
+
+        //     umap[head]++;
+        //     if(umap[head]==2) return head;
+        //     head = head->next;
+        // }
+        // return NULL;
         
     }
 };
