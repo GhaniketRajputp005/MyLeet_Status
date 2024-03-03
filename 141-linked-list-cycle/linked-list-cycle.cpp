44c8e31a9b0a8c0eaf53a7a2d5c774aa721f3f3f
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-03 13:24:56 +0530

    Time: 11 ms (44.67%) | Memory: 10.7 MB (25.44%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,18 +9,33 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,int>umap;
-        while(head!=NULL){
+
+        if(head == NULL) return false;
+        ListNode* fast = head->next;
+        ListNode* slow = head;
+
+        while(fast != slow ){
+            if(fast == NULL || fast-> next == NULL) return false;
+            slow = slow->next;
+            fast = fast->next->next;
+
+        }
+return true;
+
+//////////////// USING HASHSET ////////////
+
+        // unordered_map<ListNode*,int>umap;
+        // while(head!=NULL){
             
-            umap[head]++;
-            if(umap[head] == 2){
-                return true;
-            }
-            head = head->next;
+        //     umap[head]++;
+        //     if(umap[head] == 2){
+        //         return true;
+        //     }
+        //     head = head->next;
            
         
-        }
-        return false;
+        // }
+        // return false;
         
     }
 };
