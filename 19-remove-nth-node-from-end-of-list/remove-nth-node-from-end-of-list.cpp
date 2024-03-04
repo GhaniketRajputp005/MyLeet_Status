57a4a5a97011218691eee441fb9922b90181c343
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-04 15:27:35 +0530

    Time: 4 ms (33.74%) | Memory: 13.9 MB (14.76%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,27 +11,44 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
-        int ctr =0;
+
+        int ctr = 0;
+        unordered_map<int,ListNode*> umap;
         ListNode* temp = head;
         while(temp != NULL){
             ctr++;
+            umap[ctr] = temp;
             temp = temp->next;
         }
 
-    if( n == ctr ) {
-        head= head->next;
+        if(ctr == n){
+            head= head->next;
+            return head;
+        }
+        umap[ctr - n]->next= umap[ctr - n+1]->next;
+
         return head;
-    };
-
-        int val = ctr - n -1;
-        temp = head;
-
-        while(val--){
-            temp = temp->next;
-        }
-        temp->next = temp->next->next;
-         return head;
-
     }
+    //     int ctr =0;  O(N),o(N), two pass.
+    //     ListNode* temp = head;
+    //     while(temp != NULL){
+    //         ctr++;
+    //         temp = temp->next;
+    //     }
+
+    // if( n == ctr ) {
+    //     head= head->next;
+    //     return head;
+    // };
+
+    //     int val = ctr - n -1;
+    //     temp = head;
+
+    //     while(val--){
+    //         temp = temp->next;
+    //     }
+    //     temp->next = temp->next->next;
+    //      return head;
+
+    // }
 };
