4b4a9c04b8dfdf0c85044440954a21b7065ebee8
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-07 19:58:03 +0530

    Time: 20 ms (72.61%) | Memory: 23.7 MB (83.02%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -2,50 +2,59 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
 
-        vector<int>v;
-
-        unordered_map<int, int> umap;
-        
-        for(int i = 0; i < nums.size(); i++){
-            umap[nums[i]]++ ;
-        }
         
         int dup = 0, miss = 0;
-        
-        for(int i = 1; i <= nums.size(); i++){
-            
-            if(umap.find(i) == umap.end()) miss = i;
-            
-            if(umap[i] > 1) dup = i;
-            
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[abs(nums[i]) - 1] < 0)
+                dup = abs(nums[i]);
+            else {
+                nums[abs(nums[i]) - 1] *= -1;
+            }
         }
-        
-        v.push_back(dup);
-        v.push_back(miss);
-        return v;
-        
 
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                miss = i + 1;
+                break;
+            }
+        }
+        return {dup, miss};
 
+        // ///// O(N),O(N)
+        // unordered_map<int, int> umap;
 
+        // for(int i = 0; i < nums.size(); i++){
+        //     umap[nums[i]]++ ;
+        // }
 
+        // int dup = 0, miss = 0;
 
+        // for(int i = 1; i <= nums.size(); i++){
 
+        //     if(umap.find(i) == umap.end()) miss = i;
 
+        //     if(umap[i] > 1) dup = i;
 
+        // }
 
+        // v.push_back(dup);
+        // v.push_back(miss);
+        // return v;
 
+        ///////////   O(NLOGN)
 
-        
         // sort(nums.begin(), nums.end());
         // int flag = 0, sum = 0,i;
         // for( i  = 0; i < nums.size()-1; i++){
         //     if(nums[i] == nums [i+1] && flag == 0){
         //         v.push_back(nums[i]);
         //         flag = 1;
-               
+
         //     }
         //     sum += nums[i];
-                
+
         // }
         //  sum += nums[i];
 
@@ -54,6 +63,5 @@ public:
         // v.push_back(missingval);
 
         // return v;
-        
     }
 };
