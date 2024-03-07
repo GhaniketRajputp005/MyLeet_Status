938a840bcef7a2e9be829aba366f117dd8729cc9
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-07 22:56:57 +0530

    Time: 63 ms (96.49%) | Memory: 63.4 MB (78.44%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+
+
+  /// tortoise hare approach
+    int slow = nums[0];
+    int fast = nums[0];
+
+    ;
+
+    do{
+         slow = nums[slow];
+    fast = nums[nums[fast]];
+
+    }while( slow != fast);
+    
+    slow = nums[0];
+    while(slow != fast){
         
+        slow = nums[slow];
+        fast = nums[fast];
+        
+    }
+    
+    return fast;
 
-        int dup = 0;
-        for(int i = 0; i < nums.size(); i++){
 
-            if(nums[abs(nums[i]) - 1] < 0){
-                dup = abs(nums[i]);
-                return dup;
-            }
-            else{
-                nums[abs(nums[i]) - 1] *= -1;
-            }
-        }
-      return -1;
+
+//// approach is good but, we can't modify array so..
+
+    //     int dup = 0;
+    //     for(int i = 0; i < nums.size(); i++){
+
+    //         if(nums[abs(nums[i]) - 1] < 0){
+    //             dup = abs(nums[i]);
+    //             return dup;
+    //         }
+    //         else{
+    //             nums[abs(nums[i]) - 1] *= -1;
+    //         }
+    //     }
+    //   return -1;
     }
 };
