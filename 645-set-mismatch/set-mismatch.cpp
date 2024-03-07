a9dfdb40a0b5be1cadd7715aa94db58ac28b9045
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-07 19:20:11 +0530

    Time: 41 ms (40.78%) | Memory: 34.4 MB (19.52%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -3,24 +3,57 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
 
         vector<int>v;
-        sort(nums.begin(), nums.end());
-        int flag = 0, sum = 0,i;
-        for( i  = 0; i < nums.size()-1; i++){
-            if(nums[i] == nums [i+1] && flag == 0){
-                v.push_back(nums[i]);
-                flag = 1;
-               
-            }
-            sum += nums[i];
-                
+
+        unordered_map<int, int> umap;
+        
+        for(int i = 0; i < nums.size(); i++){
+            umap[nums[i]]++ ;
         }
-         sum += nums[i];
-
-        int actualsum = nums.size()*(nums.size()+1)/2;
-        int missingval = actualsum - (sum - v[0]);
-        v.push_back(missingval);
-
+        
+        int dup = 0, miss = 0;
+        
+        for(int i = 1; i <= nums.size(); i++){
+            
+            if(umap.find(i) == umap.end()) miss = i;
+            
+            if(umap[i] > 1) dup = i;
+            
+        }
+        
+        v.push_back(dup);
+        v.push_back(miss);
         return v;
+        
+
+
+
+
+
+
+
+
+
+
+
+        
+        // sort(nums.begin(), nums.end());
+        // int flag = 0, sum = 0,i;
+        // for( i  = 0; i < nums.size()-1; i++){
+        //     if(nums[i] == nums [i+1] && flag == 0){
+        //         v.push_back(nums[i]);
+        //         flag = 1;
+               
+        //     }
+        //     sum += nums[i];
+                
+        // }
+        //  sum += nums[i];
+
+        // int actualsum = nums.size()*(nums.size()+1)/2;
+        // int missingval = actualsum - (sum - v[0]);
+        // v.push_back(missingval);
+
+        // return v;
         
     }
 };
