879bfac8a491678367d102a5d8037019ed52990a
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-08 14:42:46 +0530

    Time: 10 ms (12.73%) | Memory: 22 MB (14.32%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -2,22 +2,41 @@ class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
         
-        unordered_map<int,int>umap;
-        int maxfreq = INT_MIN;
-        for(auto i: nums){
-            umap[i]++;
-            if(umap[i] > maxfreq){
-                maxfreq = umap[i];
+        // ONE PASS SOLN
+        vector<int>v(101,0);
+        int sum = 0, freq = 0;
+        for(auto i : nums){
+            v[i]++;
+            if(v[i] > freq ){
+                sum = v[i];
+                freq = v[i];
             }
-        }
 
-        int sum = 0;
-
-        for(auto i: umap){
-            if(i.second == maxfreq){
-                sum+= maxfreq;
+            else if(v[i] == freq){
+                sum += v[i];
             }
         }
         return sum;
+
+
+
+        ///   TWO PASS SOLN  ////
+        // unordered_map<int,int>umap;
+        // int maxfreq = INT_MIN;
+        // for(auto i: nums){
+        //     umap[i]++;
+        //     if(umap[i] > maxfreq){
+        //         maxfreq = umap[i];
+        //     }
+        // }
+
+        // int sum = 0;
+
+        // for(auto i: umap){
+        //     if(i.second == maxfreq){
+        //         sum+= maxfreq;
+        //     }
+        // }
+        // return sum;
     }
 };
