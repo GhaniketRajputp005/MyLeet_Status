eec2059429bc7f03ed3ea3c59b35b7537a74626d
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-09 21:21:26 +0530

    Time: 43 ms (79.47%) | Memory: 17.4 MB (9.36%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -2,29 +2,47 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
-        //   o(nlogn)
-        int rows = matrix.size();
-        int cols = matrix[0].size();
+        int rows = matrix.size()-1;
+        int cols = matrix[0].size()-1;
 
-        if (matrix[rows - 1][cols - 1] < target)
-            return false;
-
-        for (int i = 0; i < rows; i++) {
-
-            if (matrix[i][0] <= target && matrix[i][cols - 1] >= target) {
-                int left = 0, right = cols - 1;
-                while (left <= right) {
-                    int mid = (left + right) / 2;
-                    if (target == matrix[i][mid])
-                        return true;
-                    else if (target < matrix[i][mid])
-                        right = mid - 1;
-                    else {
-                        left = mid + 1;
-                    }
-                }
+        int i = 0;
+        while( i <= rows && cols >= 0){
+            if(matrix[i][cols] == target) return true;
+            else if(matrix[i][cols]< target){
+                i++;
+            }
+            else{
+                cols--;
             }
         }
         return false;
     }
+
+
+
+        //   o(nlogn)
+    //     int rows = matrix.size();
+    //     int cols = matrix[0].size();
+
+    //     if (matrix[rows - 1][cols - 1] < target)
+    //         return false;
+
+    //     for (int i = 0; i < rows; i++) {
+
+           
+    //             int left = 0, right = cols - 1;
+    //             while (left <= right) {
+    //                 int mid = (left + right) / 2;
+    //                 if (target == matrix[i][mid])
+    //                     return true;
+    //                 else if (target < matrix[i][mid])
+    //                     right = mid - 1;
+    //                 else {
+    //                     left = mid + 1;
+    //                 }
+    //             }
+            
+    //     }
+    //     return false;
+    // }
 };
