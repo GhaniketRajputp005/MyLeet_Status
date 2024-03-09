102587a072229861a766e98c471500275afe1562
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-09 18:35:03 +0530

    Time: 95 ms (23.25%) | Memory: 17.4 MB (41.72%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        if (matrix[rows - 1][cols - 1] < target)
+            return false;
+
+        for(int i = 0; i< rows; i++){
+
+            int left = 0, right = cols -1;
+            while(left <= right){
+                int mid = (left+right)/2;
+                if(target == matrix[i][mid])
+                    return true;
+                else if(target < matrix[i][mid])
+                    right = mid -1;
+                else{
+                    left = mid + 1;
+                }
+            }
+
+        }
+        return false;
+    }
+};
