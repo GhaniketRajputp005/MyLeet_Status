59a45dd0aa737222c3b04cadf9cda779ca9980a7
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-09 18:08:21 +0530

    Time: 8 ms (21.19%) | Memory: 11.8 MB (70.50%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-     
 
         int rows = matrix.size();
         int cols = matrix[0].size();
 
-           if(matrix[rows -1][cols-1] < target) return false;
+        if (matrix[rows - 1][cols - 1] < target)
+            return false;
 
-        int i = 0, j =  rows - 1;
+        int i = 0, j = rows - 1;
 
-        while(i <= j){
+        while (i <= j) {
 
-            int mid = (i + j)/2;
-            if(target == matrix[mid][cols - 1]) return true;
-            else if(target < matrix[mid][cols - 1])
-            {
+            int mid = (i + j) / 2;
+            if (target == matrix[mid][cols - 1])
+                return true;
+            else if (target < matrix[mid][cols - 1]) {
                 j = mid - 1;
-            }
-            else{
+            } else {
                 i = mid + 1;
             }
-
         }
 
-
-        
-        // now index i will point to location where target should be 
+        // now index i will point to location where target should be
         // present.
 
-        int left = 0, right = cols -1;
+        int left = 0, right = cols - 1;
 
-        while(left <= right){
-             int mid = (left + right)/2;
-            if(target == matrix[i][mid]) return true;
-            else if(target < matrix[i][mid])
-            {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (target == matrix[i][mid])
+                return true;
+            else if (target < matrix[i][mid]) {
                 right = mid - 1;
-            }
-            else{
+            } else {
                 left = mid + 1;
             }
         }
