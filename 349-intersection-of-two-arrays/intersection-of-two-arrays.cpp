7a03ccc02585fd2e622cd1d0e0d2fbce6c30abf7
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-10 09:17:22 +0530

    Time: 5 ms (49.43%) | Memory: 13 MB (32.08%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        // nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+       
         vector<int> v;
         unordered_map<int, int> umap;
         for (auto i : nums1) {
@@ -12,15 +12,34 @@ public:
         for (auto i : nums2) {
 
             if (umap.find(i) != umap.end()) {
-                umap[i] = -1;
+                 v.push_back(i);
+                 umap.erase(i);
             }
         }
 
-        for (auto i : umap) {
-            if (i.second == -1) {
-                v.push_back(i.first);
-            }
-        }
+        
         return v;
+
+
+        // vector<int> v;
+        // unordered_map<int, int> umap;
+        // for (auto i : nums1) {
+
+        //     umap[i]++;
+        // }
+
+        // for (auto i : nums2) {
+
+        //     if (umap.find(i) != umap.end()) {
+        //         umap[i] = -1;
+        //     }
+        // }
+
+        // for (auto i : umap) {
+        //     if (i.second == -1) {
+        //         v.push_back(i.first);
+        //     }
+        // }
+        // return v;
     }
 };
