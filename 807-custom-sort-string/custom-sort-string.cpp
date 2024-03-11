4e1b8b1bd2271c225594c084be8c7aacdaef0de4
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-11 16:34:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (39.54%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -3,32 +3,66 @@ public:
     string customSortString(string order, string s) {
         
         unordered_map<char,int>umap;
+        int ctr = 0;
         string str;
-
-        for(auto i: s){
-            umap[i]++;
-        }
-
         for(auto i: order){
-
-            if(umap.size() == 0) return str;
-            if(umap.find(i) != umap.end()){
-                while(umap[i]--){
-                    str += i;
-                }
-                umap.erase(i);
-            }
+            umap[i] = ctr++;
         }
 
-        for(auto i : umap){
+        vector<pair<int, char >>v;
 
-                while(i.second--){
-                    str += i.first;
-                }
+        for(auto i : s){
+           if(umap.find(i) != umap.end()){
+                v.push_back({umap[i],i});
+           }
+           else{
+                v.push_back({INT_MAX,i});
+           }
+        }
+
+        sort(v.begin(), v.end());
+
+        for(auto i: v){
+            str += i.second;
+        }
+         
+ return str;
+
+
+
+
+
+        ///// O(n +n + m)
+        // unordered_map<char,int>umap;
+        // string str;
+
+        // for(auto i: s){
+        //     umap[i]++;
+        // }
+
+        // for(auto i: order){
+
+        //     if(umap.size() == 0) return str;
+        //     if(umap.find(i) != umap.end()){
+        //         while(umap[i]--){  // this loop O(1) as
+        //                          //  frequency is constant
+        //             str += i;
+        //         }
+        //         umap.erase(i);
+        //     }
+        // }
+
+        // for(auto i : umap){
+
+        //         while(i.second--){
+        //             // this loop O(1) as
+        //                          //  frequency is constant
+        //             str += i.first;
+        //         }
               
 
-        }
-        return str;
+        // }
+        // return str;
 
 
     }
