f1f4e20cca17a23103635ca1f192728236deb543
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-15 21:39:03 +0530

    Time: 16 ms (59.62%) | Memory: 26.4 MB (72.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,33 +1,52 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+
         int s = nums.size();
-        vector<int> left_prod(s, 1);
-        vector<int> right_prod(s, 1);
-        vector<int> ans(nums);
+
+        vector<int> ans(s, 1);
 
         for (int i = 1; i < s; i++) {
 
-            left_prod[i] = left_prod[i - 1] * nums[i - 1];
+            ans[i] = ans[i - 1] * nums[i - 1];
         }
-        for (int i = 0; i < s; i++) {
 
-            cout << left_prod[i] << " ";
-        }
-        cout << "\n";
+        int val = nums[s - 1];
+        nums[s - 1] = 1;
 
         for (int i = s - 2; i >= 0; i--) {
+            int temp = nums[i];
+            nums[i] = nums[i + 1] * val;
 
-            right_prod[i] = right_prod[i + 1] * nums[i + 1];
+            val = temp;
         }
-        for (int i = 0; i < s; i++) {
 
-            cout << right_prod[i] << " ";
-        }
         for (int i = 0; i < s; i++) {
-            ans[i] = left_prod[i] * right_prod[i];
+            ans[i] = ans[i] * nums[i];
         }
 
         return ans;
+
+        /////////      O(N),O(N)    ///////////
+        // int s = nums.size();
+        // vector<int> left_prod(s, 1);
+        // vector<int> right_prod(s, 1);
+        // vector<int> ans(nums);
+
+        // for (int i = 1; i < s; i++) {
+
+        //     left_prod[i] = left_prod[i - 1] * nums[i - 1];
+        // }
+
+        // for (int i = s - 2; i >= 0; i--) {
+
+        //     right_prod[i] = right_prod[i + 1] * nums[i + 1];
+        // }
+
+        // for (int i = 0; i < s; i++) {
+        //     ans[i] = left_prod[i] * right_prod[i];
+        // }
+
+        // return ans;
     }
 };
