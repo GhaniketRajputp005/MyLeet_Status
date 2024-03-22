8414dcf22eda6e29bbfe09f646e55027b1d2b31c
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-03-22 19:37:58 +0530

    Time: 299 ms (5.05%) | Memory: 116.7 MB (70.02%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,48 +10,59 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
+    ListNode* reverse(ListNode* slow) {
+        // cout<<slow->val;
 
-        if (head == NULL || head->next == NULL)
-            return head;
+        if(slow == NULL) cout<<"mai NULL";
+        if (slow == NULL||slow->next == NULL)
+            return slow;
+        ListNode *before = slow, *after = slow->next;
+        before->next = NULL;
 
-        ListNode *first = head, *after = head->next;
-        first->next = NULL;
         while (after != NULL) {
             ListNode* temp = after->next;
-            after->next = first;
-            first = after;
+            after->next = before;
+            before = after;
             after = temp;
         }
 
-        return first;
+        return before;
     }
 
     bool isPalindrome(ListNode* head) {
 
+        if (head->next == NULL)
+            return true;
         ListNode *temp = head, *fast = head, *slow = head;
 
-        while (fast != NULL && fast->next!= NULL) {
-            // by doing fast->next->next!= NULL we are ensuring slow points to
-            // the right half.
-
-            slow = slow->next;
+        while (fast != NULL && fast->next != NULL) {
             fast = fast->next->next;
+            slow = slow->next;
         }
 
-        ListNode* secondhead = reverse(slow);
+        ListNode* temp1 = head;
 
-        while (secondhead != NULL) {
+      
+        while(temp1 != slow){
+            cout<< temp1->val<<" ";
+            temp1 = temp1->next;
+        }
 
-            if (secondhead->val != temp->val)
+        cout<<"slow = "<<slow->val;
+        if(slow == NULL) cout<<"kya NULL\n";
+        ListNode* head2 = reverse(slow);
+
+        while (head2 != NULL) {
+
+            if (temp->val != head2->val) {
                 return false;
+            }
 
-            secondhead = secondhead->next;
+            head2 = head2->next;
             temp = temp->next;
         }
 
-        secondhead = reverse(slow);
-
+        head2 = reverse(head2);
         return true;
     }
 };
