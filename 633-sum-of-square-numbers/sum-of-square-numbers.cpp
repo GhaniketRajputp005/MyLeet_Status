8012287614ea2dff954acf2de75ad80dbc676008
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-04-09 02:05:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (19.67%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -4,16 +4,18 @@ public:
 
        long int i = 0, j = c;
         if(c == 0) return 1;
-        while (i <= j) {
-            long int mid = (i+j) / 2;
-            cout<<mid<<endl;
-            if(mid * mid == c) return true;
-            else if (mid * mid < c) {
-                break;
-            } else if (mid * mid > c) {
-                j = mid - 1;
-            }
-        }
+        // while (i <= j) {
+        //     long int mid = (i+j) / 2;
+        //     cout<<mid<<endl;
+        //     if(mid * mid == c) return true;
+        //     else if (mid * mid < c) {
+        //         break;
+        //     } else if (mid * mid > c) {
+        //         j = mid - 1;
+        //     }
+        // }
+
+        j = sqrt(c);
 
         while (i <= j) {
             if (i * i + j * j == c) {
