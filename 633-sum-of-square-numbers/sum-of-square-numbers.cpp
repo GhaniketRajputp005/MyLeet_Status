c07b9413c5c5811587a47a73d5023292a1c4de03
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-04-09 02:04:28 +0530

    Time: 3 ms (68.78%) | Memory: 7 MB (91.98%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+
+       long int i = 0, j = c;
+        if(c == 0) return 1;
+        while (i <= j) {
+            long int mid = (i+j) / 2;
+            cout<<mid<<endl;
+            if(mid * mid == c) return true;
+            else if (mid * mid < c) {
+                break;
+            } else if (mid * mid > c) {
+                j = mid - 1;
+            }
+        }
+
+        while (i <= j) {
+            if (i * i + j * j == c) {
+                return true;
+            } else if (i * i + j * j < c) {
+                i++;
+            } else
+                j--;
+        }
+
+        return false;
+    }
+};
