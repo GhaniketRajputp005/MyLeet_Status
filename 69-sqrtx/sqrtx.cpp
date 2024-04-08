30375dfa7602d4503d2bfb3984db111ed8aa40c1
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-04-09 01:18:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (39.85%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        int l = 1, r = x, res = 0;
+
+        while (l <= r) {
+
+            long int mid = r - (r - l) / 2;
+            if (mid * mid == x)
+                return mid;
+            else if (mid * mid > x) {
+                r = mid - 1;
+            } else {
+                res = mid;
+                l = mid + 1;
+            }
+        }
+        return res;
+    }
+};
