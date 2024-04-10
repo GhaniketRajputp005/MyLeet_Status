de96f8ee2d9e2622f764b078ceee833e148b4558
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-04-10 09:39:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (67.22%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& v, int k) {
-        int n = v.size();
-        int t = 0;
-        while (true) {
-            for (int i = 0; i < n; i++){
-                if (v[i] > 0) {
-                    v[i]=v[i]-1;
-                    t++;
-                }
-            if (i == k && v[i] == 0)
-                return t;
-        }}
+        // int n = v.size();
+        // int t = 0;
+        // while (true) {
+        //     for (int i = 0; i < n; i++){
+        //         if (v[i] > 0) {
+        //             v[i]=v[i]-1;
+        //             t++;
+        //         }
+        //     if (i == k && v[i] == 0)
+        //         return t;
+        // }
+        // }
+
+
+        int tt = 0;
+
+        for(int i = 0; i < v.size(); i++){
+
+            if( i <= k)
+            tt += min(v[i], v[k]);
+            else{
+                tt += min(v[k] - 1, v[i]);
+            }
+
+           
+        }
     
-    return 0;
-    }
+    return  tt;
+    
+}
 }
 ;
