f5441fc793ef74d24305bc6202ad295d13e18c24
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-07-04 23:22:37 +0530

    Time: 413 ms (81.73%) | Memory: 261.4 MB (67.24%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -10,26 +10,43 @@
  */
 class Solution {
 public:
-    ListNode* mergeNodes(ListNode* head) {
+    // ListNode* mergeNodes(ListNode* head) {
 
-        ListNode *curr_node = head->next, *value = head,*temp;
+    //     ListNode *curr_node = head->next, *value = head,
+    //     int sum = 0;
+
+    //     while (curr_node != NULL) {
+
+    //         sum += curr_node->val;
+
+    //         if (curr_node->val == 0) {
+    //             value->val = sum;
+    //             sum = 0;
+    //             value->next = curr_node->next;
+    //             // temp = value;
+    //             value = value->next;
+    //         }
+
+    //         curr_node = curr_node->next;
+    //     }
+    //     // temp->next = NULL;
+    //     return head;
+    // }
+      ListNode* mergeNodes(ListNode* head) {
+    ListNode *curr_node = head->next, *value = head;
+        if(curr_node == NULL) return curr_node;
+
+    
         int sum = 0;
 
-        while (curr_node != NULL) {
+        while (curr_node != NULL && curr_node->val != 0) {
 
             sum += curr_node->val;
-
-            if (curr_node->val == 0) {
-                value->val = sum;
-                sum = 0;
-                value->next = curr_node->next;
-                // temp = value;
-                value = value->next;
-            }
-
             curr_node = curr_node->next;
         }
-        // temp->next = NULL;
-        return head;
+        value->val = sum;
+        value->next = mergeNodes(curr_node);
+        
+        return value;
     }
 };
