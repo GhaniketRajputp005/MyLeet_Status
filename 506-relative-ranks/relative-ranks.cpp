7fd59cd5ed149b163bc3efd7243673dcb8d90e2b
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-07-04 19:49:03 +0530

    Time: 11 ms (37.12%) | Memory: 16.2 MB (21.27%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,43 +1,81 @@
 class Solution {
 public:
-    vector<string> findRelativeRanks(vector<int>& score) {
-        
-        vector<string>answer;
-        unordered_map<int,string>u_map;
-        
+ vector<string> findRelativeRanks(vector<int>& score) {
 
-        vector<int>v(score);
+        unordered_map<int, string> u_map;
+
+        vector<int> v(score);
+        vector<string>answer;
         int n = score.size(), i = n;
 
-        if(n == 1){
-            return {"Gold Medal"};
-        }
-        else if(n == 2){
-            if(score[0] > score[1]){
-                return {"Gold Medal","Silver Medal"};
-            }
+        sort(v.begin(), v.end(),greater<int>());
+
+        for(int i = 0;i < n; i++){
+            if(i == 0) u_map[v[i]] = "Gold Medal";
+            else if(i == 1) u_map[v[i]] = "Silver Medal";
+            else if(i == 2) u_map[v[i]] = "Bronze Medal";
             else{
-                return {"Silver Medal","Gold Medal"};
+                u_map[v[i]] = to_string(i + 1);
             }
         }
 
-
-        sort(v.begin(), v.end());
-        
-       u_map[v[n - 1]] = "Gold Medal";
-       u_map[v[n - 2]] = "Silver Medal";
-       u_map[v[n - 3]] = "Bronze Medal";
-
-       while(i > 3){
-            u_map[v[n - i]] = to_string(i);
-            i--;
-       }
-
-       for(int i = 0; i < n ; i++){
+        for (int i = 0; i < n; i++) {
             answer.push_back(u_map[score[i]]);
-      
-       }
+        }
 
-    return answer;
+        return answer;
+
+
+
+
+
+//     if(a.first >= b.first){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+//    
+
+//         vector<string> answer(score .size());
+//         vector<pair<int,int>>arr_pair;
+
+//         for(int i = 0; i < score.size(); i++){
+//             arr_pair[i].first = score[i];
+//             arr_pair[i].second = i;
+//         }
+
+//         sort(arr_pair.begin(),arr_pair.end(),comp);
+
+//         for(int i = 0; i < score.size();i++){
+//             if(i==0){
+//                 answer[arr_pair[i].second] = "Gold Medal";
+//             }
+//             else if(i == 1){
+//                  answer[arr_pair[i].second] ="Silver Medal";
+//             }
+//             else if(i == 2){
+//                  answer[arr_pair[i].second] = "Bronze Medal";
+//             }
+//             else{
+//                  answer[arr_pair[i].second] = to_string(i + 1);
+//             }
+//         }
+//         return answer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 };
