8e1c11d25bccfa570d95e1e751209231621bed4a
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-07-07 00:14:50 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (96.49%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -6,16 +6,16 @@ public:
 
         if(rem_time < 0) return time + 1;
 
-        n--;
-        int complete_turns = rem_time / n;
-        int rem_person = rem_time % n;
+        
+        int complete_turns = rem_time / (n - 1);
+        int rem_person = rem_time % (n - 1);
         int answer = 0;
 
         if(complete_turns % 2 != 0){
             answer = 1 + rem_person + 1;
         }
         else{
-            answer = ++n - rem_person - 1;
+            answer = n - rem_person - 1;
         }
         return answer;
         
