15228b16c0125431aab829ecf927e05604f8ba56
Ghaniket Rajput <115147518+GhaniketRajputp005@users.noreply.github.com>
2024-09-26 14:56:42 +0530

    Time: 31 ms (70.11%) | Memory: 31.6 MB (7.18%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -4,8 +4,19 @@ public:
         
     sort(nums.begin(), nums.end());
      int s = nums.size();
+
+
+     if(nums[0] >= 0){
+        return nums[s-1] * nums[s-2] * nums[s-3];
+     }
+     else if(nums[0]  < 0 && nums[1] >0){
+        return  nums[s-1] * nums[s-2] * nums[s-3];
+     }
+     else if(nums[0] < 0 && nums[1] < 0){
      int withneg =  nums[0] * nums[1] * nums[s-1];
      int withoneg =  nums[s-1] * nums[s-2] * nums[s-3];
      return max(withneg, withoneg);
+     }
+     return 0;
     }
 };
